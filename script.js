7b25289fb34663484f8f167cd30b9ff564c29176
naoyashiga <naoyashiga0@gmail.com>
2014-12-09 23:05:52 +0900

    【add】first commit

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-location.href='https://www.google.co.jp/search?hl=ja&gs_rn=16&gs_ri=psy-ab&tok=Uvc1oHYStU0lgTDvhflcKA&suggest=p&cp=8&gs_id=f5&xhr=t&q=waste+time&bav=on.2,or.r_cp.r_qf.&bvm=bv.47534661,d.aGc&biw=1069&bih=944&um=1&ie=UTF-8&tbm=isch&source=og&sa=N&tab=wi&ei=Tgm0UaX0MaLeigf95oGADQ';
+var style = document.createElement('link');
+style.rel = 'stylesheet';
+style.type = 'text/css';
+style.href = chrome.extension.getURL('style.css');
+(document.head||document.documentElement).appendChild(style);
